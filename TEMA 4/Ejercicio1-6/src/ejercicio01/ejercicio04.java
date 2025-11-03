72ba190c97a6afa68b4b6bb4d748be805a969a48
vjp-jorgeDVG <jdevicenteg02@informatica.iesvalledeljerteplasencia.es>
2025-11-03 17:25:33 +0100

    ejercicio1-6

---
+++ TEMA 4/Ejercicio1-6/src/ejercicio01/ejercicio04.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio01;
+/**
+ *
+ * @author jorge
+ */
+
+import java.util.Scanner;
+
+public class ejercicio04 {
+
+public static void main(String[] args) {
+    
+        int num1, num2, num3;
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("introduce un numero ");
+        
+        num1 = entrada.nextInt();
+
+        System.out.println("introduce otro numero ");
+        
+        num2 = entrada.nextInt();
+
+        System.out.println("introduce el ultimo numero ");
+        
+        num3 = entrada.nextInt();
+
+        prueba.elMenor(num1, num2, num3, num3);
+    
+        }
+    }
+
