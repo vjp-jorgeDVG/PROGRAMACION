012adc7689984b41688b535964c28bf93995bf20
vjp-jorgeDVG <jdevicenteg02@informatica.iesvalledeljerteplasencia.es>
2026-02-09 14:38:58 +0100

    ejercicios

--- tema 3-3/ejercicio5/src/ejercicio5/Ejercicio5.java
+++ tema 3-3/ejercicio5/src/ejercicio5/Ejercicio5.java
@@ -3,7 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package ejercicio5;
+
 import java.util.Scanner;
+
 /**
  *
  * @author alumno
@@ -14,13 +16,14 @@ public class Ejercicio5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("introduce un numero");
         int num = sc.nextInt();
-        
-        if (num){
-            
+        if ((num % 2) == 0) {
+            System.out.println("numero par");
+        } else {
+            System.out.println("numero impar");
         }
     }
-    
+
 }
